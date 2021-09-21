42aa9c716c7c7c63c20a62e8f8742a43e102329c
irackson <nasirus@protonmail.com>
2021-09-21 15:55:16 -0400

    add middleware and formatting files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 
 const cors = require('cors');
@@ -8,17 +8,17 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-
 // Middleware
 app.use(express.json());
 app.use(cors());
 
+//! enable method override
+app.use(methodOverride('_method'));
 
+//! serves the public folder as static
+app.use(express.static('public'));
 
-
-
-
-
-
+//! urlencoded into order to receive data from forms
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
